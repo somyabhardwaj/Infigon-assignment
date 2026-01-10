4380c9a3f31230cfe7852df96d77b70d353763b1
Somya bhardwaj <somyabhardwaj@Somyas-MacBook-Air.local>
2026-01-10 15:36:57 +0530

    error handler

---
+++ pages/HomePage.tsx
@@ -0,0 +1,23 @@
+export default async function HomePage(){
+try{
+   return(
+    <div>
+        <div>
+            <h2>
+
+            </h2>
+            <p>
+
+            </p>
+        </div>
+    </div>
+   )
+}
+catch(error){
+  return(
+     <div>
+        
+     </div>
+  )
+}
+}
